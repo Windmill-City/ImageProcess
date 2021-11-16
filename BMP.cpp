37640e5a379a9bd25165f656d14813dc2c50939e
Windmill-City <1449182174@qq.com>
2021-11-16 11:23:30 +0800

    增加傅里叶变换函数

--- BMP.cpp
+++ BMP.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <iostream>
 #include <fstream>
+#include <complex>
 
 //(BM)0x424D
 #define BITMAP_FILE_TYPE 0x4D42
@@ -307,3 +308,21 @@ void Equalize() {
 		}
 	}
 }
+
+#define PI (3.1415926535)
+
+void FT(std::complex<double>* TD, std::complex<double>* FD, int len) {
+	int x, u;
+	double angle;
+	for (u = 0; u < len; u++) 
+	{
+		FD[u] = 0;
+		for (x = 0; x < len; x++)
+		{
+			angle = -2 * PI * u * x / len;
+			FD[u] += TD[x] * std::complex<double>(cos(angle), sin(angle));
+		}
+		FD[u] /= len;
+	}
+}
+
